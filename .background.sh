88a93b480777f60e4144c239f908dc66f357b925
toku-sa-n <tokuchan3515@gmail.com>
2019-03-22 07:44:14 +0900

    Deleted translucent.

--- .background.sh
+++ .background.sh
@@ -9,13 +9,9 @@ if [ ! -d $IMG_DIR ]; then
         mkdir -v "$IMG_DIR"
     fi
 else
-    while true
-    do
-        if [ "$(ls -A $IMG_DIR)" ]; then
-            ls $IMG_DIR|sort -R|tail -1|while read IMGFILE; do
-                feh --bg-fill "$IMG_DIR/$IMGFILE";
-            done
-        fi
-        sleep 5m
-    done
+    if [ "$(ls -A $IMG_DIR)" ]; then
+        ls $IMG_DIR|sort -R|tail -1|while read IMGFILE; do
+            feh --bg-fill "$IMG_DIR/$IMGFILE";
+        done
+    fi
 fi
