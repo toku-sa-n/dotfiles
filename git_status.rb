e03fe8a14e3a99b11661b7162da5d54f9779fd6c
toku-sa-n <tokuchan3515@gmail.com>
2019-06-06 19:40:24 +0900

    Integrated similar process into a function.

--- git_status.rb
+++ git_status.rb
@@ -1,9 +1,21 @@
 #!/usr/bin/ruby
 
+def print_git_status(locations, push_or_commit, color)
+  print "<span color=\"##{color}\">Need to #{push_or_commit}: "
+  location_msg = ''
+  locations.each do |location|
+    location_msg += location + ', '
+  end
+  # [0...-2] is intended to delete the last of ', '
+  print location_msg[0...-2]
+  print '</span> '
+end
+
 # first split into each line, then split into two elements, status and place.
 git_status = `git-remind status -s`.split("\n").map do |status|
   status.split(' ')
 end
+
 need_to_commit = []
 need_to_push = []
 git_status.each do |status|
@@ -13,24 +25,6 @@ git_status.each do |status|
     need_to_push.push(status[1])
   end
 end
-unless need_to_commit.empty?
-  print '<span color="#ff9900">Need to commit: '
-  status_msg = ''
-  need_to_commit.each do |status|
-    status_msg += status + ', '
-  end
-  # [0...-2] is intended to delete the last of ', '
-  print status_msg[0...-2]
-  print '</span> '
-end
 
-unless need_to_push.empty?
-  print '<span color="#ff0000">Need to push: '
-  status_msg = ''
-  need_to_push.each do |status|
-    status_msg += status + ', '
-  end
-  # [0...-2] is intended to delete the last of ', '
-  print status_msg[0...-2]
-  print '</span>'
-end
+print_git_status(need_to_commit, 'commit', 'ff9900') unless need_to_commit.empty?
+print_git_status(need_to_push, 'push', 'ff0000') unless need_to_push.empty?
