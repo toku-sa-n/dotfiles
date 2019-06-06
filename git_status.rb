eac8336b2df7e6e1f3c73ec70fda49d98a2999d6
toku-sa-n <tokuchan3515@gmail.com>
2019-06-06 18:47:38 +0900

    Changed the way to display.

--- git_status.rb
+++ git_status.rb
@@ -1,15 +1,43 @@
 #!/usr/bin/ruby
 
 # first split into each line, then split into two elements, status and place.
-git_status = `git-remind status -s`.split("\n").map do |status|
+raw_git_status = `git-remind status -s`.split("\n").map do |status|
   status.split(' ')
 end
+git_status = []
+raw_git_status.each do |status|
+  new_element = {}
+  new_element[:status] = status[0]
+  new_element[:location] = status[1]
+  git_status.push(new_element)
+end
+need_to_commit = []
+need_to_push = []
 git_status.each do |status|
-  status[1].gsub!(/#{ENV['HOME']}/, '~')
-  if status[0] == 'C'
-    print '<span color="#ff9900">Need to commit: '
+  if status[:status] == 'C'
+    need_to_commit.push(status)
   else
-    print '<span color="#ff0000">Need to push: '
+    need_to_push.push(status)
   end
-  print "#{status[1]}</span> "
+end
+unless need_to_commit.empty?
+  print '<span color="#ff9900">Need to commit: '
+  status_msg = ''
+  need_to_commit.each do |status|
+    status_msg += (status[:location]).to_s + ', '
+  end
+  # [0...-2] is intended to delete the last of ', '
+  print status_msg[0...-2].to_s
+  print '</span>'
+end
+
+unless need_to_push.empty?
+  print '<span color="#ffff00">Need to push: '
+  status_msg = ''
+  need_to_commit.each do |status|
+    status_msg += (status[:location]).to_s + ', '
+  end
+  # [0...-2] is intended to delete the last of ', '
+  print status_msg[0...-2].to_s
+  print '</span>'
 end
