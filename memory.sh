8c1797894fb77c2246b9896e97db1243c613bcfa
toku-sa-n <tokuchan3515@gmail.com>
2019-06-12 08:48:11 +0900

    Fix the bug of not colorized memory bar..

--- memory.sh
+++ memory.sh
@@ -15,11 +15,13 @@ BLOCKS=$(seq ${BLOCK_NUM}|tr -d '0\n'|tr '[1-9]' '@')
 SPACES=$(seq ${SPACE_NUM}|tr -d '0\n'|tr '[1-9]' ' ')
 BAR="${BLOCKS}${SPACES}"
 
-case "${BLOCKS}" in
-    <1-2>  ) COLOR= "#ffffff" ;;
-    <3-4>  ) COLOR= "#00ff00" ;;
-    <5-6>  ) COLOR= "#ffff00" ;;
-    <7-8>  ) COLOR= "#ff9900" ;;
-    <->    ) COLOR= "#ff0000" ;;
+COLOR=""
+case "${BLOCK_NUM}" in
+    <1-2>  ) COLOR="#ffffff" ;;
+    <3-4>  ) COLOR="#00ff00" ;;
+    <5-6>  ) COLOR="#ffff00" ;;
+    <7-8>  ) COLOR="#ff9900" ;;
+    <->    ) COLOR="#ff0000" ;;
 esac
+
 echo -n "\"full_text\": \"${USING_MEMORY_READABLE} / ${FULL_MEMORY_READABLE} [${BAR}]  ${USE_RATE}%\", \"color\": \"${COLOR}\""
