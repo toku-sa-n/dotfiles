9ccaf5e120a945caf97c1541b12dbc954338f8ee
toku-sa-n <tokuchan3515@gmail.com>
2019-06-28 22:08:39 +0900

    No longer use git-remind command.
    It is bothering to install git-remind command.

--- git_status.rb
+++ git_status.rb
@@ -8,7 +8,7 @@ class GitRepository
     Dir.chdir(path) do
       git_status = `git status -sb`
       @need_to_commit = git_status.lines.count > 1
-      @need_to_push = if git_status =~ /\[ahead\] [0-9]+/
+      @need_to_push = if git_status =~ /\[ahead [0-9]+\]/
                         true
                       else
                         false
@@ -24,37 +24,37 @@ class GitRepository
     @need_to_push
   end
 
-  def show_path
+  def path
     @path.gsub(/#{ENV['HOME']}/, '~')
   end
 end
 
-def print_git_status(locations, push_or_commit, color)
+def print_git_status(repositories, push_or_commit, color)
   print "<span color=\"##{color}\">Need to #{push_or_commit}: "
   location_msg = ''
-  locations.each do |location|
-    location_msg += location + ', '
+  repositories.each do |repository|
+    location_msg += repository.path + ', '
   end
   # [0...-2] is intended to delete the last of ', '
   print location_msg[0...-2]
   print '</span> '
 end
 
-# first split into each line, then split into two elements, status and place.
-git_status = `git-remind status -s`.split("\n").map do |status|
-  status.split(' ')
-end
+def main
+  need_to_commit = []
+  need_to_push = []
 
-need_to_commit = []
-need_to_push = []
-git_status.each do |status|
-  status[1].gsub!(/#{ENV['HOME']}/, '~')
-  if status[0] == 'C'
-    need_to_commit.push(status[1])
-  else
-    need_to_push.push(status[1])
+  `git config --global remind.paths`.chomp.split(',').each do |path|
+    git_repository = GitRepository.new(path)
+    if git_repository.need_to_push?
+      need_to_push << git_repository
+    elsif git_repository.need_to_commit?
+      need_to_commit << git_repository
+    end
   end
+
+  print_git_status(need_to_commit, 'commit', 'ff9900') unless need_to_commit.empty?
+  print_git_status(need_to_push, 'push', 'ff0000') unless need_to_push.empty?
 end
 
-print_git_status(need_to_commit, 'commit', 'ff9900') unless need_to_commit.empty?
-print_git_status(need_to_push, 'push', 'ff0000') unless need_to_push.empty?
+main
