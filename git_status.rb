5f0e84f0c5074c114118bbc5a366c87dcf16bcab
toku-sa-n <tokuchan3515@gmail.com>
2019-06-28 21:46:14 +0900

    Add GitRepository class.

--- git_status.rb
+++ git_status.rb
@@ -1,4 +1,33 @@
 #!/usr/bin/ruby
+# frozen_string_literal: true
+
+class GitRepository
+  def initialize(path)
+    @path = path
+
+    Dir.chdir(path) do
+      git_status = `git status -sb`
+      @need_to_commit = git_status.lines.count > 1
+      @need_to_push = if git_status =~ /\[ahead\] [0-9]+/
+                        true
+                      else
+                        false
+                    end
+    end
+  end
+
+  def need_to_commit?
+    @need_to_commit
+  end
+
+  def need_to_push?
+    @need_to_push
+  end
+
+  def show_path
+    @path.gsub(/#{ENV['HOME']}/, '~')
+  end
+end
 
 def print_git_status(locations, push_or_commit, color)
   print "<span color=\"##{color}\">Need to #{push_or_commit}: "
