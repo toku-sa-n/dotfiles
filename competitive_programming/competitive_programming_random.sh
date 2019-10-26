eeb323617e412ef2bcf68ee21a721ea6417d7fa0
toku-sa-n <tokuchan3515@gmail.com>
2019-10-26 14:30:40 +0900

    [Add]Show the number of problems to solve

--- competitive_programming/competitive_programming_random.sh
+++ competitive_programming/competitive_programming_random.sh
@@ -1,3 +1,19 @@
 #!/bin/zsh
 
+past_date=$(sed '1!d' ./records.txt)
+count_problem=$(sed '2!d' ./records.txt)
+
+diff_date=$(( ($(date +'%s') - $past_date)/86400))
+
+count_problem=$(( $count_problem+4*$diff_date ))
+
+echo "残り${count_problem}問"
 shuf competitive_programming_problem.txt|head -n1|xargs firefox &
+
+if [[ $count_problem -gt 0 ]];
+then
+    ((count_problem--))
+fi
+
+date +'%s' > records.txt
+echo $count_problem >> records.txt
