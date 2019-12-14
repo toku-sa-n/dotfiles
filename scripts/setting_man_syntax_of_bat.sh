d8c9320bbbb6dbc1786df67fdbb564447a6476c8
toku-sa-n <42336339+toku-sa-n@users.noreply.github.com>
2019-12-14 22:02:35 +0900

    [Refactoring]Move manpage setting file
    setting_man_syntax_of_bat.sh uses this file.

--- scripts/setting_man_syntax_of_bat.sh
+++ scripts/setting_man_syntax_of_bat.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 mkdir -p $HOME/.config/bat/syntaxes
-ln -s $HOME/dotfiles/Man.sublime-syntax $HOME/.config/bat/syntaxes/Man.sublime-syntax
+ln -s $HOME/dotfiles/scripts/Man.sublime-syntax $HOME/.config/bat/syntaxes/Man.sublime-syntax
 
 bat cache --build
