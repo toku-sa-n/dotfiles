5b3c94daa782e2046a54074f1f63308e34bcb278
toku-sa-n <tokuchan3515@gmail.com>
2020-01-06 11:56:42 +0900

    [Change]Run upgrade_kernel.sh inside upgrade_gentoo.sh
    Copy-paste is bad

--- scripts/upgrade_gentoo.sh
+++ scripts/upgrade_gentoo.sh
@@ -11,26 +11,7 @@ fi
 emerge-webrsync
 emerge --ask --verbose --tree --update --deep --newuse --keep-going --with-bdeps=y @world
 
-# $HOME is not the home directory of the user who runs sudo.
-# I hate direct path so copied kernel_upgrade.sh.
-
-# if only one version of Linux kernel is available, exit.
-if [[ $(eselect kernel list|wc -l) != 2 ]]; then
-
-    MAKEOPTS="-j$(nproc||echo 8)"
-    BACKUP_KERNEL_PATH=$HOME/kernel-config-$(uname -r)
-    # back up the old kernel config.
-    cp /usr/src/linux-$(uname -r)/.config $BACKUP_KERNEL_PATH
-
-    LATEST_KERNEL_ESELECT_OPTION=$(eselect kernel list|tail -n1|awk '{print $1}'|tr -d '\[\]')
-
-    eselect kernel set $LATEST_KERNEL_ESELECT_OPTION
-
-    cp /usr/src/linux-$(uname -r)/.config /usr/src/linux/.config
-
-    # use `&&' not to install corrupt kernel.
-    cd /usr/src/linux && make $MAKEOPTS && make modules_install $MAKEOPTS && make install && genkernel --install initramfs && grub-mkconfig -o /boot/grub/grub.cfg
-fi
+./upgrade_kernel.sh
 
 emerge --ask --depclean
 
