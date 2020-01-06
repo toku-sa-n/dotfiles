3f2b37ea26a21da0a11d7b3a04343b5fc8e52744
toku-sa-n <tokuchan3515@gmail.com>
2020-01-06 11:57:36 +0900

    [Refactoring]Delete unnecessary comments

--- scripts/upgrade_gentoo.sh
+++ scripts/upgrade_gentoo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Immediately abort to run this script when an error occurs.
 set -e
 
 if [[ $(whoami) != "root" ]]; then
@@ -16,9 +15,3 @@ emerge --ask --verbose --tree --update --deep --newuse --keep-going --with-bdeps
 emerge --ask --depclean
 
 emerge @preserved-rebuild
-
-# $HOME is "root's" home directory so non-system-wise upgrade can't be run.
-
-# command -v pip > /dev/null && pip list --outdated --format=freeze | awk -F '=' '{print $1}' | xargs pip install --upgrade --user
-# command -v zplug > /dev/null && zplug update
-# command -v gem > /dev/null && gem update
