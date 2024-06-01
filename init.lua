c52da24d0b567a72716b20a8d8181fd8a028ef2a
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-01 23:04:44 +0900

    Adding

--- init.lua
+++ init.lua
@@ -86,4 +86,30 @@ require("lazy").setup({
 		end,
 	},
 	"nvim-lua/plenary.nvim",
+	{
+		"voldikss/vim-floaterm",
+		config = function()
+			vim.api.nvim_set_keymap("n", "<Leader>g", ":FloatermNew lazygit<CR>", { noremap = true, silent = true })
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			local mappings = {
+				{ "n", "<leader>a", ":Gwrite<CR>" },
+				{ "n", "<leader>c", ":Git commit<CR>" },
+				{ "n", "<up>", ":Git push<CR>" },
+				{ "n", "<down>", ":Git pull<CR>" },
+				{ "c", "gck<space>", "Git checkout<space>" },
+				{ "c", "gckb<space>", "Git checkout -b<space>" },
+				{ "c", "gm<space>", "Git merge<space>" },
+				{ "c", "gb<space>", "Git branch<space>" },
+				{ "c", "gbd<space>", "Git branch -d<space>" },
+			}
+
+			for _, maps in ipairs(mappings) do
+				vim.api.nvim_set_keymap(maps[1], maps[2], maps[3], { noremap = true, silent = true })
+			end
+		end,
+	},
 })
