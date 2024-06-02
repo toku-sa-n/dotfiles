0a7f671fed4d2b7a443a3b27adad55888fd4739d
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 23:04:38 +0900

    OK

--- neovim/init.lua
+++ neovim/init.lua
@@ -2,8 +2,6 @@ vim.scriptencoding = "utf-8"
 
 vim.g.mapleader = " "
 
--- {{{ Plugins
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
