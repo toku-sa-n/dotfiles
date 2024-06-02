fe1efed059c15b51afa1981fe707aaca45a3b329
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 18:56:52 +0900

    Split

---
+++ neovim/init.lua
@@ -0,0 +1,20 @@
+vim.scriptencoding = "utf-8"
+
+vim.g.mapleader = " "
+
+-- {{{ Plugins
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
--- neovim/lua/plugins/plugins.lua
+++ neovim/lua/plugins/plugins.lua
@@ -1,21 +1,4 @@
-vim.scriptencoding = "utf-8"
-
-vim.g.mapleader = " "
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
+return {
 	{
 		"nvim-tree/nvim-tree.lua",
 		init = function()
@@ -231,4 +214,4 @@ require("lazy").setup({
 		end,
 	},
 	"whonore/Coqtail",
-})
+}
