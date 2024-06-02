039f73c87faca06583fffd5212c2c40c3cbce686
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 23:13:12 +0900

    OK

--- neovim/lua/plugins/plugins.lua
+++ neovim/lua/plugins/plugins.lua
@@ -1,15 +1,4 @@
 return {
-	{
-		"w0ng/vim-hybrid",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("hybrid")
-			vim.g.hybrid_custom_term_colors = 1
-			vim.g.hybrid_reduced_contrast = 1
-		end,
-		opts = {},
-	},
 	"nvim-tree/nvim-web-devicons",
 	"github/copilot.vim",
 	{
---
+++ neovim/lua/plugins/vim-hybrid.lua
@@ -0,0 +1,11 @@
+return {
+	"w0ng/vim-hybrid",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("hybrid")
+		vim.g.hybrid_custom_term_colors = 1
+		vim.g.hybrid_reduced_contrast = 1
+	end,
+	opts = {},
+}
