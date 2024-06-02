edcaff3fcf3828b763bf47a0efca6cb95c535e4d
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 11:55:53 +0900

    Adding

--- init.lua
+++ init.lua
@@ -56,6 +56,9 @@ require("lazy").setup({
 		config = function()
 			vim.g.autoformat_autoindent = 0
 			vim.api.nvim_create_autocmd({ "BufWritePre" }, { command = "Autoformat" })
+
+			vim.g.formatdef_hs = '"(hindent | stylish-haskell)"'
+			vim.g.formatters_haskell = { "hs" }
 		end,
 	},
 	{
