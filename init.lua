e4d5093423ec97f64f30030ba32b1b166caafa9e
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 11:20:07 +0900

    Doing

--- init.lua
+++ init.lua
@@ -44,6 +44,8 @@ require("lazy").setup({
 		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme("hybrid")
+			vim.g.hybrid_custom_term_colors = 1
+			vim.g.hybrid_reduced_contrast = 1
 		end,
 		opts = {},
 	},
