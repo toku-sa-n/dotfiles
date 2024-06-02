27d59060e547e042dda9007bc6f1eb0ad2cf3b70
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 23:15:39 +0900

    Split

--- neovim/lua/plugins/plugins.lua
+++ neovim/lua/plugins/plugins.lua
@@ -1,15 +1,5 @@
 return {
 	{
-		"vim-autoformat/vim-autoformat",
-		config = function()
-			vim.g.autoformat_autoindent = 0
-			vim.api.nvim_create_autocmd({ "BufWritePre" }, { command = "Autoformat" })
-
-			vim.g.formatdef_hs = '"(hindent | stylish-haskell)"'
-			vim.g.formatters_haskell = { "hs" }
-		end,
-	},
-	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			"folke/neodev.nvim",
