eed1065cda732e6454e3fa57c1790d8a7e0fd837
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 23:32:27 +0900

    Split

--- neovim/lua/plugins/plugins.lua
+++ neovim/lua/plugins/plugins.lua
@@ -1,10 +1,4 @@
 return {
-	{
-		"mikeboiko/vim-markdown-folding",
-		config = function()
-			vim.g.vim_markdown_folding_disabled = 1
-		end,
-	},
 	"is0n/jaq-nvim",
 	"thinca/vim-quickrun",
 	{
---
+++ neovim/lua/plugins/vim-markdown-folding.lua
@@ -0,0 +1,7 @@
+return {
+	"mikeboiko/vim-markdown-folding",
+	config = function()
+		vim.g.vim_markdown_folding_disabled = 1
+	end,
+	ft = "markdown",
+}
