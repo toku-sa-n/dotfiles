3aa8d8bc1518400e9fb3284c9a513850be48dc2a
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 23:10:11 +0900

    Add

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -1,11 +1,9 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "ayu_dark",
-			},
-		})
-	end,
+	opts = {
+		options = {
+			theme = "ayu_dark",
+		},
+	},
 }
