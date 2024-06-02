91ac0a61669509c0afc6f20dda77c08ed7599fbf
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 23:06:49 +0900

    Remove

--- neovim/lua/plugins/plugins.lua
+++ neovim/lua/plugins/plugins.lua
@@ -1,16 +1,5 @@
 return {
 	{
-		"nvim-tree/nvim-tree.lua",
-		init = function()
-			vim.g.loaded_netrw = 1
-			vim.g.loaded_netrwPlugin = 1
-		end,
-		config = function()
-			vim.api.nvim_set_keymap("n", "<F9>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-			require("nvim-tree").setup({})
-		end,
-	},
-	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
