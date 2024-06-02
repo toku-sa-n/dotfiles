bfa3284851daf04f7092fd4936f527108ac4a09f
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 23:36:46 +0900

    Split

--- neovim/lua/plugins/plugins.lua
+++ neovim/lua/plugins/plugins.lua
@@ -1,11 +1,4 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
-	"numToStr/Comment.nvim",
 	"vim-jp/vimdoc-ja",
 	{
 		"kylechui/nvim-surround",
