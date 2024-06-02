069fbf1f4918e93349a1c8d1aad6f0ebfc4f9ea9
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-02 11:46:05 +0900

    Adding

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-vim.o.scriptencoding = "utf-8"
+vim.scriptencoding = "utf-8"
 
 vim.g.mapleader = " "
 
@@ -150,14 +150,36 @@ require("lazy").setup({
 		config = function()
 			local cmp = require("cmp")
 			cmp.setup({
-				expand = function(args)
-					vim.snippet.expand(args.body)
-				end,
+				snippet = {
+					expand = function(args)
+						vim.snippet.expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+					["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "buffer" },
 				}),
 			})
+
+			local key_dir = {
+				["<Tab>"] = 1,
+				["<S-Tab>"] = -1,
+			}
+
+			for key, dir in pairs(key_dir) do
+				vim.keymap.set({ "i", "s" }, key, function()
+					if vim.snippet.active({ direction = dir }) then
+						return "<cmd>lua vim.snippet.jump(" .. dir .. ")<cr>"
+					else
+						return "<Tab>"
+					end
+				end, { expr = true })
+			end
 		end,
 	},
 	"folke/neodev.nvim",
