baad42f460f65f6ff70e0fc366df2b528e165a25
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-03 13:39:14 +0900

    Fix

--- neovim/lua/plugins/vim-hybrid.lua
+++ neovim/lua/plugins/vim-hybrid.lua
@@ -1,12 +1,10 @@
 return {
-	"chriskempson/base16-vim",
+	"w0n*/vim-hybrid",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.g.base16colorspace = 256
-		vim.opt.termguicolors = true
-		vim.cmd.colorscheme("base16-default-dark")
-		-- vim.g.hybrid_custom_term_colors = 1
-		-- vim.g.hybrid_reduced_contrast = 1
+		vim.cmd.colorscheme("hybrid")
+		vim.g.hybrid_custom_term_colors = 1
+		vim.g.hybrid_reduced_contrast = 1
 	end,
 }
