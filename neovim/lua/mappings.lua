df68f7559a32d29ca7414c279d0ab57cc3c88af0
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-03 16:25:59 +0900

    Add

--- neovim/lua/mappings.lua
+++ neovim/lua/mappings.lua
@@ -9,6 +9,8 @@ local nmaps = {
 	{ "N", "Nzz" },
 	{ "*", "*zz" },
 	{ "#", "#zz" },
+	{ "<leader>b", ":bprevious<CR>" },
+	{ "<leader>n", ":bnext<CR>" },
 	{ "<leader>w", ":update<CR>" },
 }
 
@@ -16,9 +18,12 @@ for _, map in ipairs(nmaps) do
 	vim.api.nvim_set_keymap("n", map[1], map[2], { noremap = true, silent = true })
 end
 
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>.",
-	":tabnew " .. os.getenv("MYVIMRC"):gsub("\\", "\\\\"):gsub(" ", "\\ ") .. "<CR>",
-	{ noremap = true, silent = true }
-)
+local vimrc_path = (function()
+	local path = os.getenv("MYVIMRC")
+
+	return path and vim.fn.escape(vim.fn.resolve(path), "\\ ")
+end)()
+
+if vimrc_path then
+	vim.api.nvim_set_keymap("n", "<leader>.", ":tabnew " .. vimrc_path .. "<CR>", { noremap = true, silent = true })
+end
