fba36f6422a93356acca48d535b3869ed78d08c6
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-03 13:55:14 +0900

    Fix

--- neovim/lua/plugins/vim-hybrid.lua
+++ neovim/lua/plugins/vim-hybrid.lua
@@ -1,10 +1,10 @@
 return {
-	"w0n*/vim-hybrid",
+	"w0ng/vim-hybrid",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("hybrid")
 		vim.g.hybrid_custom_term_colors = 1
 		vim.g.hybrid_reduced_contrast = 1
+		vim.cmd.colorscheme("hybrid")
 	end,
 }
