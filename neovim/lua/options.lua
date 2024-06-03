411e00e907cd1a5ed1380b9e295d5542f387e374
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-03 18:10:19 +0900

    Options

---
+++ neovim/lua/options.lua
@@ -0,0 +1,11 @@
+vim.opt.relativenumber = true
+vim.opt.showmatch = true
+vim.opt.matchtime = 5
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.gdefault = true
+
+vim.opt.matchpairs:append("「:」")
+
+vim.opt.fileencodings:append({ "utf-8", "sjis" })
