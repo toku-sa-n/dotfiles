02745587892aa58b13e85d8b72577d344a1eac0b
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-05 09:38:51 +0900

    Add a shortcut

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -10,6 +10,13 @@ return {
 			vim.api.nvim_buf_set_keymap(
 				bufnr,
 				"n",
+				"gd",
+				"<cmd>lua vim.lsp.buf.definition()<CR>",
+				{ noremap = true, silent = true }
+			)
+			vim.api.nvim_buf_set_keymap(
+				bufnr,
+				"n",
 				"K",
 				"<cmd>lua vim.lsp.buf.hover()<CR>",
 				{ noremap = true, silent = true }
