845b36163bb43bf56634b63b35e671ca846e08f9
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-13 09:31:32 +0900

    Use the Neovim version of a colorscheme

--- .config/nvim/lua/plugins/vim-hybrid.lua
+++ .config/nvim/lua/plugins/vim-hybrid.lua
@@ -1,10 +1,10 @@
 return {
-	"w0ng/vim-hybrid",
+	"rafi/neo-hybrid.vim",
 	lazy = false,
 	priority = 1000,
 	config = function()
 		vim.g.hybrid_custom_term_colors = 1
 		vim.g.hybrid_reduced_contrast = 1
-		vim.cmd.colorscheme("hybrid")
+		vim.cmd.colorscheme("neohybrid")
 	end,
 }
