289f3f887f742850624fc16881dfc6efe76591a0
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-15 02:11:18 +0900

    Lazily-load `vim-fugitive`

--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,21 +1,15 @@
 return {
 	"tpope/vim-fugitive",
-	config = function()
-		local mappings = {
-			{ "n", "<leader>a", ":Gwrite<CR>" },
-			{ "n", "<leader>c", ":Git commit<CR>" },
-			{ "n", "<up>", ":Git push<CR>" },
-			{ "n", "<down>", ":Git pull<CR>" },
-			{ "c", "gck<space>", "Git checkout<space>" },
-			{ "c", "gckb<space>", "Git checkout -b<space>" },
-			{ "c", "gm<space>", "Git merge<space>" },
-			{ "c", "gb<space>", "Git branch<space>" },
-			{ "c", "gbd<space>", "Git branch -d<space>" },
-		}
-
-		for _, maps in ipairs(mappings) do
-			vim.api.nvim_set_keymap(maps[1], maps[2], maps[3], { noremap = true, silent = true })
-		end
-	end,
+	keys = {
+		{ "<leader>a", "<cmd>Gwrite<CR>" },
+		{ "<leader>c", "<cmd>Git commit<CR>" },
+		{ "<up>", "<cmd>Git push<CR>" },
+		{ "<down>", "<cmd>Git pull<CR>" },
+		{ "gck<space>", "Git checkout<space>", mode = "c" },
+		{ "gckb<space>", "Git checkout -b<space>", mode = "c" },
+		{ "gm<space>", "Git merge<space>", mode = "c" },
+		{ "gb<space>", "Git branch<space>", mode = "c" },
+		{ "gbd<space>", "Git branch -d<space>", mode = "c" },
+	},
 	event = { "BufRead", "BufNewFile" },
 }
