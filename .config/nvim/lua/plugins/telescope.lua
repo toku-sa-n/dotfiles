6be10032e0fe018d3f8e6216d67078e542c30015
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-15 02:09:21 +0900

    Lazily load `telescope.nvim`

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,14 +3,8 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
-	config = function()
-		vim.api.nvim_set_keymap(
-			"n",
-			"<Leader>f",
-			":Telescope find_files hidden=true no_ignore=false<CR>",
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_set_keymap("n", "<Leader>b", ":Telescope buffers<CR>", { noremap = true, silent = true })
-	end,
-	event = "VeryLazy",
+	keys = {
+		{ "<leader>f", "<cmd>Telescope find_files hidden=true no_ignore=false<CR>" },
+		{ "<leader>b", "<cmd>Telescope buffers<CR>" },
+	},
 }
