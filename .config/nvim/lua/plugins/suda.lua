1e4ada616d38eba8ba12e0beb01fc19e9991d72a
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-15 02:08:26 +0900

    Lazy

--- .config/nvim/lua/plugins/suda.lua
+++ .config/nvim/lua/plugins/suda.lua
@@ -1,7 +1,6 @@
 return {
 	"lambdalisue/vim-suda",
-	config = function()
-		vim.api.nvim_set_keymap("n", "<leader>W", ":SudaWrite<CR>", { noremap = true, silent = true })
-	end,
-	event = { "BufNewFile", "BufRead" },
+	keys = {
+		{ "<leader>W", "<cmd>SudaWrite<CR>" },
+	},
 }
