a078b64553566cb81dc37f32c5c7ad40c7295a50
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-19 09:52:13 +0900

    Invoke `:Git` with `<leader>g`

--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -3,6 +3,7 @@ return {
 	keys = {
 		{ "<leader>a", "<cmd>Gwrite<CR>" },
 		{ "<leader>c", "<cmd>Git commit<CR>" },
+		{ "<leader>g", "<cmd>Git<CR>" },
 		{ "<up>", "<cmd>Git push<CR>" },
 		{ "<down>", "<cmd>Git pull<CR>" },
 		{ "gck<space>", "Git checkout<space>", mode = "c" },
