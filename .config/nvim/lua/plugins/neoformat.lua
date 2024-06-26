0a555bc0b8ce68534313c533a19d8a24221e27f5
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-26 16:29:26 +0900

    Do not `undojoin`

--- .config/nvim/lua/plugins/neoformat.lua
+++ .config/nvim/lua/plugins/neoformat.lua
@@ -2,10 +2,7 @@ return {
 	"sbdchd/neoformat",
 	config = function()
 		vim.api.nvim_create_augroup("neoformat", {})
-		vim.api.nvim_create_autocmd(
-			"BufWritePre",
-			{ group = "neoformat", pattern = "*", command = "undojoin | Neoformat" }
-		)
+		vim.api.nvim_create_autocmd("BufWritePre", { group = "neoformat", pattern = "*", command = "Neoformat" })
 	end,
 	event = "InsertEnter",
 }
