bf0f73bc5d2f240dd7023c4be6f639d6c216d91a
Hiroki Tokunaga <tokusan441@gmail.com>
2024-07-04 23:18:18 +0900

    Use my fork

--- .config/nvim/lua/plugins/neovim-hybrid.lua
+++ .config/nvim/lua/plugins/neovim-hybrid.lua
@@ -1,10 +1,10 @@
 return {
-	"rafi/neo-hybrid.vim",
+	"toku-sa-n/neovim-hybrid",
 	lazy = false,
 	priority = 1000,
 	config = function()
 		vim.g.hybrid_custom_term_colors = 1
 		vim.g.hybrid_reduced_contrast = 1
-		vim.cmd.colorscheme("neohybrid")
+		vim.cmd.colorscheme("hybrid")
 	end,
 }
