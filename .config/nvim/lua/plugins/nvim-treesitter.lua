7b3dc1144d1598c975d95a60b8634377c210b249
Hiroki Tokunaga <tokusan441@gmail.com>
2024-07-10 13:45:22 +0900

    Fix

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,16 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = "all",
-		highlight = {
-			enable = true,
-			disable = { "haskell" },
-		},
-		additional_vim_regex_highlighting = { "haskell" },
-	},
-	config = true,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = "all",
+			highlight = {
+				enable = true,
+				disable = { "haskell" },
+			},
+			additional_vim_regex_highlighting = { "haskell" },
+		})
+	end,
 	event = { "BufRead", "BufNewFile" },
 
 	-- Neovim complains about missing a Lua parser if this plugin is loaded
