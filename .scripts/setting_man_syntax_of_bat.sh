ae84eb5ecf0acaedd8baf53314f7b39ddff5cf42
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-05 10:27:01 +0900

    refactor: remove unused files

--- .scripts/setting_man_syntax_of_bat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-readonly SCRIPT_DIR=$(cd $(dirname $0); pwd)
-
-mkdir -p $HOME/.config/bat/syntaxes
-ln -s {$SCRIPT_DIR,$HOME/.config/bat/syntaxes}/Man.sublime-syntax
-
-bat cache --build
