8c0fc6aba0cc38a03469768fb221fe9ac6837d20
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-07 16:01:31 +0900

    feat: enable `compinit`

--- .zshrc.d/envvar.zsh
+++ .zshrc.d/envvar.zsh
@@ -11,7 +11,11 @@ export GPG_TTY=$(tty)
 export WORDCHARS='_'
 [[ -r "$HOME/.config/bat/syntaxes/Man.sublime-syntax" ]] && export MANPAGER="sh -c 'sed -e s/.\\\\x08//g|bat -l man -p'"
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh && export FZF_DEFAULT_COMMAND='\find .'
-[[ ! -r $HOME/.opam/opam-init/init.zsh ]] || source $HOME/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
+[[ ! -r $HOME/.opam/opam-init/init.zsh ]] || source $HOME/.opam/opam-init/init.zsh >/dev/null 2>/dev/null
+
+fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
+autoload -U compinit
+compinit
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS+=(regexp)
 ZSH_HIGHLIGHT_REGEXP+=('^\s*(\.){2,}$' fg=green,bold)
