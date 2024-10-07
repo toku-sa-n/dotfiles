0c477ded01feb5c4ce4da5c9a7511b6d7b14cdf4
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-07 11:48:53 +0900

    refactor: simplify

--- .zshrc.d/distro_spec.zsh
+++ .zshrc.d/distro_spec.zsh
@@ -1,14 +1,4 @@
-gentoo(){
+if [[ -f /etc/gentoo-release ]]; then
     alias inst='sudo emerge -avt'
     alias unst='sudo emerge -cav'
-}
-
-# An anonymous function is used to hide the `dist` variable.
-(){
-    readonly local dist=$(cat /etc/*-release|grep '^ID'|awk -F '[=]' '{print $2}')
-
-    case $dist in
-        "gentoo" ) gentoo;;
-    esac
-}
-
+fi
