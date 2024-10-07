e0382ea67ea50515bee2c3a2f493e896baf87e0a
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-07 11:44:10 +0900

    refactor: I won't use ArchLinux anymore

--- .zshrc.d/distro_spec.zsh
+++ .zshrc.d/distro_spec.zsh
@@ -1,12 +1,3 @@
-arch(){
-    alias detailpac='pacman -Qi'
-    alias ns='sudo netctl start'
-    alias inst='sudo pacman -S'
-    alias unst='sudo pacman -Rs'
-    alias upgr='yes|sudo pacman -Syu && common_upgr'
-    alias paclist=$'pacman -Ql|awk -F " " \'{print $1}\'|uniq|less'
-}
-
 gentoo(){
     alias inst='sudo emerge -avt'
     alias unst='sudo emerge -cav'
@@ -17,7 +8,6 @@ gentoo(){
     readonly local dist=$(cat /etc/*-release|grep '^ID'|awk -F '[=]' '{print $2}')
 
     case $dist in
-        "arch" ) arch ;;
         "gentoo" ) gentoo;;
     esac
 }
