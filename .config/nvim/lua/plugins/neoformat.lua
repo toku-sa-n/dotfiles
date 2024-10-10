9c70bf6d6ed0d6201a44232bba5a7688cfd9e321
Hiroki Tokunaga <tokusan441@gmail.com>
2024-10-10 16:45:26 +0900

    feat(neoformat): use `prettier` for markdown files

--- .config/nvim/lua/plugins/neoformat.lua
+++ .config/nvim/lua/plugins/neoformat.lua
@@ -3,6 +3,8 @@ return {
 	config = function()
 		vim.api.nvim_create_augroup("neoformat", {})
 		vim.api.nvim_create_autocmd("BufWritePre", { group = "neoformat", pattern = "*", command = "Neoformat" })
+
+		vim.g.neoformat_enabled_markdown = { "prettier" }
 	end,
 	event = "InsertEnter",
 }
