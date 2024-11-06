35152a1475ecb1b5838a93b568e2a80ea335a053
Hiroki Tokunaga <tokusan441@gmail.com>
2024-11-06 15:02:23 +0900

    feat!: do not load a colorscheme for man pages

--- .zshrc.d/envvar.zsh
+++ .zshrc.d/envvar.zsh
@@ -9,7 +9,7 @@ export HISTSIZE=1000
 export SAVEHIST=1000
 export GPG_TTY=$(tty)
 export WORDCHARS='_'
-[[ -r "$HOME/.config/bat/syntaxes/Man.sublime-syntax" ]] && export MANPAGER="sh -c 'sed -e s/.\\\\x08//g|bat -l man -p'"
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh && export FZF_DEFAULT_COMMAND='\find .'
 [[ ! -r $HOME/.opam/opam-init/init.zsh ]] || source $HOME/.opam/opam-init/init.zsh >/dev/null 2>/dev/null
 
