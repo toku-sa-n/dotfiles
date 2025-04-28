adabe2f6e44967690a3d40077b2e32c4610ea561
Hiroki Tokunaga <tokusan441@gmail.com>
2025-04-29 00:05:59 +0900

    feat(neovim): auto-format Rocq files

--- .config/nvim/lua/plugins/neoformat.lua
+++ .config/nvim/lua/plugins/neoformat.lua
@@ -4,7 +4,13 @@ return {
 		vim.api.nvim_create_augroup("neoformat", {})
 		vim.api.nvim_create_autocmd("BufWritePre", { group = "neoformat", pattern = "*", command = "Neoformat" })
 
+		vim.g.neoformat_coq_coqfmt = {
+			exe = "coqfmt",
+			stdin = true,
+		}
+
 		vim.g.neoformat_enabled_markdown = { "prettier" }
+		vim.g.neoformat_enabled_coq = { "coqfmt" }
 	end,
 	event = "InsertEnter",
 }
